d713005a14d46cd4802d34bdefc497c9897283e0
Poxito <aritzplazaola3@gmail.com>
2022-10-28 11:10:19 +0200

    DIP printzipioa eginda

---
+++ SOLID/src/dip/Deduction.java
@@ -0,0 +1,8 @@
+package dip;
+
+public class Deduction {
+
+	public int calcDeduction(String sex, String year) {
+		return 1;
+	}
+}
---
+++ SOLID/src/dip/Preconditions.java
@@ -0,0 +1,10 @@
+package dip;
+
+import java.util.HashMap;
+
+public class Preconditions {
+
+	public boolean isPosible(String subject, HashMap<String, Integer> subjectRecord) {
+		return true;
+	}
+}
